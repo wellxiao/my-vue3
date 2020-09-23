c8dbcb50b26f3a4803bb283b67bb62c7598c4770
wellxiao <xiaowei504@icloud.com>
2020-09-23 10:51:39 +0800

    第一次上传

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <hello-world msg="Hello Vue 3.0 + Vite" />
+  </div>
+</template>
+
+<script lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
